22e12ef3af86232b1c5765a2ea97ab02576a4327
FrancoisLaferriere <flaferriere@uni-potsdam.de>
2021-05-19 20:56:29 +0200

    clean

--- qasp/experiments/result_qasp/runtest_qasp.sh
+++ qasp/experiments/result_qasp/runtest_qasp.sh
@@ -21,8 +21,8 @@ run(){
         FILE=${!INSTANCES[i]:0:1}
         N=${!INSTANCES[i]:1:1}
         P=${!INSTANCES[i]:2:1}
-        echo "---------$i: $FILE -c n=$N -c p=$P --------------"
-        echo "---------$i: $FILE -c n=$N -c p=$P --------------" >> $OUT
+        echo "---------$((i+1)): $FILE -c n=$N -c p=$P --------------"
+        echo "---------$((i+1)): $FILE -c n=$N -c p=$P --------------" >> $OUT
         { time timeout $TIMEOUT bash -c "qasp2qbf.py --pipe $META $QUANT $DIR$FILE -c n=$N -c p=$P" ; } >> $OUT 2>&1
         check_timeout >> $OUT
     done
